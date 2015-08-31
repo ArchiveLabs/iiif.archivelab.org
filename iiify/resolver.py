8340a81211cedf2de9c5bb9e6599d3fb1850f265
Michael E. Karpeles <michael.karpeles@gmail.com>
2015-08-30 20:23:16 -0700

    it works

--- iiify/resolver.py
+++ iiify/resolver.py
@@ -6,7 +6,7 @@ from configs import options, cors, approot, cache_root, media_root
 
 archive = 'https://archive.org'
 bookdata = '%s/BookReader/BookReaderJSON.php' % (archive)
-# BookReader/BookReaderImages.php
+bookreader = "%s/BookReader/BookReaderImages.php" % (archive)
 
 
 def ia_resolver(identifier):
@@ -17,6 +17,9 @@ def ia_resolver(identifier):
     fetches it, if it doesn't exist on disk..
     """
     path = os.path.join(media_root, identifier)
+    leaf = None
+    if "$" in identifier:
+        identifier, leaf = identifier.split("$")
     metadata = requests.get('%s/metadata/%s' % (archive, identifier)).json()
     server = metadata.get('server', 'https://archive.org')
     subPrefix = metadata['dir']
@@ -32,20 +35,11 @@ def ia_resolver(identifier):
                              stream=True, allow_redirects=True)
 
         elif mediatype.lower() == 'texts':
-            f = next(f['name'] for f in files if
-                     f['name'].endswith('_jp2.zip') and
-                     f['format'] == 'Single Page Processed JP2 ZIP')
-        
-            r = requests.get(bookdata, data={
-                    'server': server,
-                    'itemPath': subPrefix,
-                    'itemId': identifier
-                    }).json()
-
-            #r = requests.get('%s/%s' % (archive, bookreader),
-            #                 stream=True, allow_redirects=True)
+            if not leaf:
+                raise
+            r = requests.get('%s/download/%s/page/%s' % (archive, identifier, leaf))
 
         with open(path, 'wb') as rc:
             rc.writelines(r.iter_content(chunk_size=1024))
 
-    return os.path.join(media_root, identifier)
+    return path
