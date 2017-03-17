91899e93f8380b4f89445c4718cc30d5bce4f21a
Mek Karpeles <michael.karpeles@gmail.com>
2017-03-17 01:07:22 +0000

    fixing #20

---
+++ iiify/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    __init__.py
+    ~~~~~~~~~~~
+
+    :copyright: (c) 2017 Archive Labs.
+    :license: see LICENSE for more details.
+"""
+
+__title__ = 'ia-iiif'
+__version__ = '0.0.2'
+__author__ = 'mek'
