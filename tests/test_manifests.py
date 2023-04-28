09d29765ba8fe27286249101e788a9388142231c
Glen Robson <glen.robson@gmail.com>
2023-04-28 23:45:38 +0100

    Adding an initial version of a v3 manifest

---
+++ tests/test_manifests.py
@@ -0,0 +1,20 @@
+import unittest
+from flask.testing import FlaskClient
+from iiify.app import app
+
+class TestManifests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.test_app = FlaskClient(app)
+
+    def test_v3_image_manifest(self):
+        resp = self.test_app.get("/iiif/3/rashodgson68/manifest.json")
+        self.assertEqual(resp.status_code, 200)
+        manifest = resp.json
+
+        self.assertEqual(manifest['type'], "Manifest", f"Unexpected type. Expected Manifest go {manifest['type']}")
+        self.assertEqual(len(manifest['items']),32,f"Expected 32 canvases but got: {len(manifest['items'])}")
+
+
+if __name__ == '__main__':
+    unittest.main()
