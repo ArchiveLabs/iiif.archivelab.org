9173d10bb6397ac6b2f2884611e3cd1832f5667b
Mike Bennett <mike@digitaldogsbody.net>
2023-05-25 22:17:02 +0100

    Metadata first pass

--- iiify/resolver.py
+++ iiify/resolver.py
@@ -219,6 +219,22 @@ def create_manifest3(identifier, domain=None, page=None):
     if "description" in metadata["metadata"]:
         manifest.summary = {"none": [metadata["metadata"]["description"]]}
 
+    excluded_fields = [
+    'avg_rating', 'backup_location', 'btih', 'description', 'downloads',
+    'imagecount', 'indexflag', 'item_size', 'licenseurl'
+    'noindex', 'num_reviews', 'oai_updatedate', 'publicdate', 'publisher',  'reviewdate',
+    'scanningcentre', 'stripped_tags', 'uploader'
+    ]
+
+    manifest_metadata = []
+
+    for field in metadata["metadata"]:
+        if field not in excluded_fields:
+            manifest_metadata.append(
+                {"label": {"none": [field]}, "value": {"none": [str(metadata["metadata"][field])]}})
+
+    manifest.metadata = manifest_metadata
+
     if mediatype == 'texts':
         # Get bookreader metadata (mostly for filenames and height / width of image)
         bookReaderURL = f"https://{metadata.get('server')}/BookReader/BookReaderJSIA.php?id={identifier}&itemPath={metadata.get('dir')}&server={metadata.get('server')}&format=jsonp&subPrefix={identifier}"
