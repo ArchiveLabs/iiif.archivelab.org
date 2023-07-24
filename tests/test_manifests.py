2122e04b5c60f96cdd5d1c0d1d5e8d253b2f4e4e
Josh Hadro <josh.hadro@gmail.com>
2023-07-24 16:55:20 -0400

    Add some tests

--- tests/test_manifests.py
+++ tests/test_manifests.py
@@ -39,5 +39,29 @@ class TestManifests(unittest.TestCase):
 
         self.assertEqual(len(manifest['items']),116,f"Expected 116 canvas but got: {len(manifest['items'])}")
 
+    def test_v3_single_video_manifest(self):
+        resp = self.test_app.get("/iiif/3/youtube-7w8F2Xi3vFw/manifest.json")
+        self.assertEqual(resp.status_code, 200)
+        manifest = resp.json
+
+        self.assertEqual(len(manifest['items']),1,f"Expected 1 canvas but got: {len(manifest['items'])}")
+
+    #logic to cover etree mediatype github issue #123
+    def test_v3_etree_mediatype(self):
+        resp = self.test_app.get("/iiif/3/gd72-04-14.aud.vernon.23662.sbeok.shnf/manifest.json")
+        self.assertEqual(resp.status_code, 200)
+        manifest = resp.json
+
+        self.assertEqual(len(manifest['items']),36,f"Expected 1 canvas but got: {len(manifest['items'])}")
+
+''' to test:
+TvQuran.com__Alafasi (64Kbps MP3)
+alice_in_wonderland_librivox (128kbps mp3)
+kaled_jalil (no derivatives)
+taboca_201002_03 (h.264 MPEG4, OGG Theora)
+Dokku_obrash (geo-restricted?)
+
+'''
+
 if __name__ == '__main__':
     unittest.main()
