130bf47851fb922d5180fa606263896cfe601081
Glen Robson <glen.robson@gmail.com>
2023-07-27 22:42:54 +0100

    Fixing failing test

--- tests/test_manifests_v2.py
+++ tests/test_manifests_v2.py
@@ -12,5 +12,7 @@ class TestManifests(unittest.TestCase):
         self.assertEqual(resp.status_code, 200)
         manifest = resp.json
 
-        self.assertEqual(manifest['@type'], "Manifest", f"Unexpected type. Expected Manifest got {manifest['@type']}")
+        self.assertEqual(manifest['@id'], 'https://iiif.archivelab.org/iiif/rashodgson68/manifest.json', 'V2 Manifest ID has changed')
+        self.assertEqual(manifest['@type'], "sc:Manifest", f"Unexpected type. Expected Manifest got {manifest['@type']}")
         self.assertEqual(len(manifest['sequences'][0]['canvases']),32,f"Expected 32 canvases but got: {len(manifest['sequences'][0]['canvases'])}")
+        self.assertEqual(manifest['sequences'][0]['canvases'][0]['@id'],"https://iiif.archivelab.org/iiif/img-8664_202009/canvas",f"v2 canvas id has changed")
